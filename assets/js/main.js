02a2b466f38bf7725ead7b1e2bf293e67211e02c
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-24 15:16:07 +0100

    Aggiunto ciclo che verifica se l'utente ha scritto un numero
    Aggiunto ciclo che verifica se l'utente ha scritto un numero

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,10 +53,19 @@ for (var i= 0; i<= possibilita; i++) {
     console.log("hai perso");
     break;
     }
+    //ciclo che verifica se l'utente ha scritto un numero
+    while (numeroUtente != Number(numeroUtente)) {
+        alert(" Devi inserire un numero");
+        numeroUtente = prompt("Inserisci un numero da 1 a 100");
+    };
     //ciclo che verifica se il numero inserito è compreso tra 1 e 100
     while(numeroUtente < 1 || numeroUtente > 100){
         alert(" Il numero non è compreso tra 1 e 100");
         numeroUtente = prompt("Inserisci un numero da 1 a 100");
+        while (numeroUtente != Number(numeroUtente)) {
+            alert(" Devi inserire un numero");
+            numeroUtente = prompt("Inserisci un numero da 1 a 100");
+        }
     }
     //ciclo che verifica che l'utente non ripeta lo stesso numero
     while(arrayIndexOf(listaNumeriUtente, numeroUtente) !== -1) {
@@ -65,6 +74,10 @@ for (var i= 0; i<= possibilita; i++) {
         while(numeroUtente < 1 || numeroUtente > 100){
             alert(" Il numero non è compreso tra 1 e 100");
             numeroUtente = prompt("Inserisci un numero da 1 a 100");
+            while (numeroUtente != Number(numeroUtente)) {
+                alert(" Devi inserire un numero");
+                numeroUtente = prompt("Inserisci un numero da 1 a 100");
+            }
         }
     }
     listaNumeriUtente.push(numeroUtente);
