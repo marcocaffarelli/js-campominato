de2a4e388cf8d9c64054d1ec0e4fdbab08eb7b36
marcocaffarelli <caffarelli-m94@hotmail.it>
2020-11-25 18:22:52 +0100

    Esercizio bonus pulito il codice
    Esercizio bonus pulito il codice

--- Bonus/assets/js/main.js
+++ Bonus/assets/js/main.js
@@ -1,3 +1,33 @@
+///////////////////////////////////////////// Lista variabilii /////////////////////////////////////////////
+var sceltaUtente, difficolta, listaSedici, num, listaNumeriUtente, possibilita, i, numeroUtente
+////////////////////////////////////////////// lista funzioni //////////////////////////////////////////////
+// funzione che verifica la presenza di un elemento all'interno di un array
+function arrayIndexOf(array, search) {
+    var indice; 
+    for (indice = 0; indice < array.length; indice++) {
+        if (array[indice] == search)
+            return indice;
+    }	
+    return -1;
+};
+
+//creata funzione contenente le casistiche in cui l'utente scrive qualcosa di strano
+function erroriUtente(numero, lista, opzioni) {
+while ((numero != Number(numero)) || (numero < 1 || numero > opzioni) || (arrayIndexOf(lista, numero) !== -1)) {
+    if (numero != Number(numero)) {
+        alert(" Devi inserire un numero");
+        numero = prompt("Inserisci un numero diverso")
+    } else if(numero < 1 || numero > opzioni){
+        alert(" Il numero non è compreso nella lista");
+        numero = prompt("Inserisci un numero diverso");
+    } else if(arrayIndexOf(lista, numero) !== -1){
+        alert(" Il numero è già stato inserito, inserisci un numero diverso");
+        numero = prompt("Inserisci un numero diverso");
+    }
+}
+    return numero
+};
+
 // scelta livello difficolta 
 sceltaUtente = prompt("Scrivi un livello di difficolta tra quelli consigliati", "FACILE-MEDIO-DIFFICILE");
 sceltaUtente = sceltaUtente.toUpperCase();
@@ -5,36 +35,25 @@ difficolta = sceltaUtente;
 
 switch (difficolta) {
     case "DIFFICILE":
-        // Il computer deve generare 16 numeri casuali tra 1 e 50.
+    // Il computer deve generare 16 numeri casuali tra 1 e 50.
     listaSedici = [];
-    var num;
     //ciclo for per generre 16 numeri casuali
     for(i= 0; i <= 15; i++){
         while ( listaSedici.includes( num = Math.floor(Math.random()*50) + 1 ) );
         listaSedici[i] = num;
     };
     //console.log(listaSedici);
-    
-    // funzione che verifica la presenza di un elemento all'interno di un array
-    function arrayIndexOf(array, search) {
-        var indice; 
-        for (indice = 0; indice < array.length; indice++) {
-            if (array[indice] == search)
-                return indice;
-        }	
-        return -1;
-    };
-    
+
     //lista numeri inseriti dall'utente
     listaNumeriUtente = [];
     
-    //var possibilita = 50 - 16;
-    var possibilita = 33;
+    //var possibilita 
+    possibilita = 33;
     
     //ciclo for che genera il gioco
-    for (var i= 0; i<= possibilita; i++) {
-        var numeroUtente = prompt("Inserisci un numero da 1 e 50");
-        numeroUtente = erroriUtenteDifficile(numeroUtente, listaNumeriUtente);
+    for (i= 0; i<= possibilita; i++) {
+        numeroUtente = prompt("Inserisci un numero da 1 e 50");
+        numeroUtente = erroriUtente(numeroUtente, listaNumeriUtente, 50);
         if (arrayIndexOf(listaSedici, numeroUtente) !== -1) {
         alert("Hai perso");
         break;
@@ -48,58 +67,31 @@ switch (difficolta) {
     } else {
         alert(listaNumeriUtente.length + " questo è il numero delle volte che hai evitato una bomba");
     };
-    
-    //creata funzione contenente le casistiche in cui l'utente scrive qualcosa diverso di un numero compreso tra 1 e 100
-    function erroriUtenteDifficile(numero, lista) {
-        while ((numero != Number(numero)) || (numero < 1 || numero > 50) || (arrayIndexOf(lista, numero) !== -1)) {
-            if (numero != Number(numero)) {
-                alert(" Devi inserire un numero");
-                numero = prompt("Inserisci un numero da 1 e 50")
-            } else if(numero < 1 || numero > 50){
-                alert(" Il numero non è compreso tra 1 e 50");
-                numero = prompt("Inserisci un numero da 1 e 50");
-            } else if(arrayIndexOf(lista, numero) !== -1){
-                alert(" Il numero è già stato inserito, inserisci un numero diverso");
-                numero = prompt("Inserisci un numero da 1 e 50");
-            }
-        }
-        return numero
-    };
-        break;
+
+    break;
     ////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
     case "MEDIO":
    // Il computer deve generare 16 numeri casuali tra 1 e 100.
     listaSedici = [];
-    var num;
-    //ciclo for per generre 16 numeri casuali
+    //ciclo for per generare 16 numeri casuali
     for(i= 0; i <= 15; i++){
-        while ( listaSedici.includes( num = Math.floor(Math.random()*100) + 1 ) );
+        while ( listaSedici.includes( num = Math.floor(Math.random()*80) + 1 ) );
         listaSedici[i] = num;
     };
     //console.log(listaSedici);
 
-    // funzione che verifica la presenza di un elemento all'interno di un array
-    function arrayIndexOf(array, search) {
-        var indice; 
-        for (indice = 0; indice < array.length; indice++) {
-            if (array[indice] == search)
-                return indice;
-        }	
-        return -1;
-    };
-
     //lista numeri inseriti dall'utente
     listaNumeriUtente = [];
 
-    //var possibilita = 80 - 16;
-    var possibilita = 83;
+    //var possibilita
+    possibilita = 63;
 
     //ciclo for che genera il gioco
-    for (var i= 0; i<= possibilita; i++) {
-        var numeroUtente = prompt("Inserisci un numero da 1 e 80");
-        numeroUtente = erroriUtenteMedio(numeroUtente, listaNumeriUtente);
+    for (i= 0; i<= possibilita; i++) {
+        numeroUtente = prompt("Inserisci un numero da 1 e 80");
+        numeroUtente = erroriUtente(numeroUtente, listaNumeriUtente, 80);
         if (arrayIndexOf(listaSedici, numeroUtente) !== -1) {
         alert("Hai perso");
         break;
@@ -108,28 +100,12 @@ switch (difficolta) {
     };
 
     // Aggiunti messaggi a seconda dell'esito del gioco
-    if (listaNumeriUtente.length === 84) {
+    if (listaNumeriUtente.length === 64) {
         alert("Complimenti sei riuscito a completare il Gioco");
     } else {
         alert(listaNumeriUtente.length + " questo è il numero delle volte che hai evitato una bomba");
     };
 
-    //creata funzione contenente le casistiche in cui l'utente scrive qualcosa diverso di un numero compreso tra 1 e 100
-    function erroriUtenteMedio(numero, lista) {
-        while ((numero != Number(numero)) || (numero < 1 || numero > 80) || (arrayIndexOf(lista, numero) !== -1)) {
-            if (numero != Number(numero)) {
-                alert(" Devi inserire un numero");
-                numero = prompt("Inserisci un numero da 1 e 80")
-            } else if(numero < 1 || numero > 80){
-                alert(" Il numero non è compreso tra 1 e 80");
-                numero = prompt("Inserisci un numero da 1 e 80");
-            } else if(arrayIndexOf(lista, numero) !== -1){
-                alert(" Il numero è già stato inserito, inserisci un numero diverso");
-                numero = prompt("Inserisci un numero da 1 e 80");
-            }
-        }
-        return numero
-    };
     break;
     ////////////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////////////
@@ -137,7 +113,6 @@ switch (difficolta) {
     case "FACILE":
     // Il computer deve generare 16 numeri casuali tra 1 e 100.
     listaSedici = [];
-    var num;
     //ciclo for per generre 16 numeri casuali
     for(i= 0; i <= 15; i++){
         while ( listaSedici.includes( num = Math.floor(Math.random()*100) + 1 ) );
@@ -145,26 +120,16 @@ switch (difficolta) {
     };
     //console.log(listaSedici);
 
-    // funzione che verifica la presenza di un elemento all'interno di un array
-    function arrayIndexOf(array, search) {
-        var indice; 
-        for (indice = 0; indice < array.length; indice++) {
-            if (array[indice] == search)
-                return indice;
-        }	
-        return -1;
-    };
-
     //lista numeri inseriti dall'utente
     listaNumeriUtente = [];
 
-    //var possibilita = 100 - 16;
-    var possibilita = 83;
+    //var possibilita
+    possibilita = 83;
 
     //ciclo for che genera il gioco
-    for (var i= 0; i<= possibilita; i++) {
-        var numeroUtente = prompt("Inserisci un numero da 1 e 100");
-        numeroUtente = erroriUtenteFacile(numeroUtente, listaNumeriUtente);
+    for (i= 0; i<= possibilita; i++) {
+        numeroUtente = prompt("Inserisci un numero da 1 e 100");
+        numeroUtente = erroriUtente(numeroUtente, listaNumeriUtente, 100);
         if (arrayIndexOf(listaSedici, numeroUtente) !== -1) {
         alert("Hai perso");
         break;
@@ -177,23 +142,6 @@ switch (difficolta) {
         alert("Complimenti sei riuscito a completare il Gioco");
     } else {
         alert(listaNumeriUtente.length + " questo è il numero delle volte che hai evitato una bomba");
-    };
-
-    //creata funzione contenente le casistiche in cui l'utente scrive qualcosa diverso di un numero compreso tra 1 e 100
-    function erroriUtenteFacile(numero, lista) {
-        while ((numero != Number(numero)) || (numero < 1 || numero > 100) || (arrayIndexOf(lista, numero) !== -1)) {
-            if (numero != Number(numero)) {
-                alert(" Devi inserire un numero");
-                numero = prompt("Inserisci un numero da 1 e 100")
-            } else if(numero < 1 || numero > 100){
-                alert(" Il numero non è compreso tra 1 e 100");
-                numero = prompt("Inserisci un numero da 1 e 100");
-            } else if(arrayIndexOf(lista, numero) !== -1){
-                alert(" Il numero è già stato inserito, inserisci un numero diverso");
-                numero = prompt("Inserisci un numero da 1 e 100");
-            }
-        }
-        return numero
     };
     break;
     default:
